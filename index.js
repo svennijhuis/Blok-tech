6250b5dda4e0a6948331dc014b97b8f236b49658
svennijhuis <sanijhuis@live.nl>
2022-02-17 21:24:51 +0100

    Deze week heb ik gewerkt aan Node.js en het verder uitbreiden hiervan.

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3000;
+//Loads the handlebars module
+const handlebars = require('express-handlebars');
+//Sets our app to use the handlebars engine
+app.set('view engine', 'hbs');
+//Sets handlebars configurations (we will go through them later on)
+app.engine('hbs', handlebars.engine({
+layoutsDir: `${__dirname}/views/layouts`,
+extname: 'hbs',
+defaultLayout: 'index'
+}));
+
+app.use(express.static('static'))
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+res.render('main');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get('/about', (req, res) => {
+  res.render('about');
+  });
+
+
+app.listen(port, () => console.log(`App listening to port ${port}`));
+
+
+
+
