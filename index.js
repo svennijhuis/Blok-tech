f3c87607b71ac5219ec83ad5d52e73d48c27c3ef
svennijhuis <sanijhuis@live.nl>
2022-03-09 16:02:13 +0100

    Voeg env map toe

--- index.js
+++ index.js
@@ -6,27 +6,52 @@ const handlebars = require('express-handlebars');
 //Sets our app to use the handlebars engine
 app.set('view engine', 'hbs');
 //Sets handlebars configurations (we will go through them later on)
-app.engine('hbs', handlebars.engine({
-layoutsDir: `${__dirname}/views/layouts`,
-extname: 'hbs',
-defaultLayout: 'index'
-}));
+app.engine(
+	'hbs',
+	handlebars.engine({
+		layoutsDir: `${__dirname}/views/layouts`,
+		extname: 'hbs',
+		defaultLayout: 'index',
+	})
+);
+
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@schoolproject.fn6hb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+let userCollection;
+// connect database
+client.connect(err => {
+	// fill user collection
+	userCollection = client.db('userInfo').collection('users');
+	console.log('connection made');
+});
 
 // css-img
-app.use(express.static('static'))
+app.use(express.static('static'));
+app.use(express.json()); //Used to parse JSON bodies
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/createUser', (req, res) => {
+	console.log(req.body);
+	const name = req.body.name;
+	const surname = req.body.surname;
+	userCollection.insertOne({ name: name, surname: surname });
+});
 
 // home
-app.get('/', (req, res) => {
-res.render('main');
+app.get('/',  (req, res) => {
+	res.render('main');
 });
 // about
-app.get('/about', (req, res) => {
-  res.render('about');
-  });
-
+app.get('/about', async (req, res) => {
+	const users = await userCollection.find().toArray();
+	console.log(users);
+	res.render('about', { users: users });
+});
 
 app.get('*', (req, res) => {
-  res.render('errors');
+	res.render('errors');
 });
 
 app.listen(port, () => console.log(`App listening to port ${port}`));
