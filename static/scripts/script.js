d493587e83773aeda6eb0b63229c000b732abd65
svennijhuis <sanijhuis@live.nl>
2022-03-17 01:03:28 +0100

    aanpassingen css

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,11 +1,26 @@
-// voorwaarde button
-const terms = document.querySelector('#term');
-const btn = document.querySelector('.button-aanmelden');
+/*** light and dark mode ***/
 
-terms.onclick = () => {
-    if(terms.checked){
-        btn.disabled = false;
-} else{
-    btn.disabled = treu;
-    }
-};
+const btn = document.querySelector(".btn-toggle");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+const bodyDarkLight = document.querySelector("body");
+const currentTheme = localStorage.getItem("theme");
+const lightTheme = "light-theme";
+const darkTheme = "dark-theme";
+let theme;
+
+if (currentTheme == "dark") {
+  bodyDarkLight.classList.toggle(darkTheme);
+} else if (currentTheme == "light") {
+  bodyDarkLight.classList.toggle(lightTheme);
+}
+
+btn.addEventListener("click", () => {
+  if (prefersDarkScheme.matches) {
+    bodyDarkLight.classList.toggle(lightTheme);
+    theme = bodyDarkLight.classList.contains(lightTheme) ? "light" : "dark";
+  } else {
+    bodyDarkLight.classList.toggle(darkTheme);
+    theme = bodyDarkLight.classList.contains(darkTheme) ? "dark" : "light";
+  }
+  localStorage.setItem("theme", theme);
+});
